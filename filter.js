0f4b339fe437924c96d61aeadc0f2ec55987e6a0
melongz <1945857873@qq.com>
2017-07-04 19:10:29 +0800

    canvas-filter

--- filter.js
+++ filter.js
@@ -1,36 +1,102 @@
-const filter = Object.values({
-    origin(){},
-    filter1(imgData){
+
+var filter = Object.values({
+
+    // 原图
+    origin(imgData){},
+
+    // 曝光
+    light(imgData){
         let d = imgData.data;
-        for(var i=0; i<d.length; i+=4){
+        for (var i = 0; i < d.length; i += 4) {
             d[i] += 50 ;
-            d[i+1] += 50 ;
-            d[i+2] += 50 ;
+            d[i + 1] += 50 ;
+            d[i + 2] += 50 ;
         }
     },
-    filter2(imgData){
+
+    // 变暗
+    dark(imgData){
         let d = imgData.data;
-        for(var i=0; i<d.length; i+=4){
+        for (var i = 0; i < d.length; i += 4) {
             d[i] -= 50 ;
-            d[i+1] -= 50 ;
-            d[i+2] -= 50 ;
+            d[i + 1] -= 50 ;
+            d[i + 2] -= 50 ;
         }
     },
-    filter3(imgData){
+
+    // 暖色
+    bianhong(imgData) {
         let d = imgData.data;
-        for(var i=0; i<d.length; i+=4){
-            d[i] += 50 ;
+        for (var i = 0; i < d.length; i += 4) {
+            d[i] += 30;
         }
     },
-    filter4(imgData){
+
+    // 反色
+    colorInvertProcess(imgData) {
         let d = imgData.data;
-        for(var i=0; i<d.length; i+=4){
-            let r = d[i] ,
-                g = d[i+1],
-                b = d[i+2];
-            d[i] = 255 -r;
-            d[i+1] = 255- g;
-            d[i+2] = 255 - b;
+        for (var i = 0; i < d.length; i += 4) {
+            var r = d[i];
+            var g = d[i + 1];
+            var b = d[i + 2];
+
+            d[i] = 255-r;
+            d[i + 1] = 255-g;
+            d[i + 2] = 255-b;
         }
     },
+
+    // 黑白
+    blackAndWhite(imgData) {
+        let d = imgData.data;
+        for (var i = 0; i < d.length; i += 4) {
+            var r = d[i];
+            var g = d[i + 1];
+            var b = d[i + 2];
+
+            var v = 0.2126*r + 0.7152*g + 0.0722*b;
+            d[i] = d[i+1] = d[i+2] = v;
+        }
+    },
+
+
+    // 复古
+    sepiaFilter(imgData){
+        let d = imgData.data;
+        for (let i = 0; i < d.length; i += 4) {
+            let r = d[i]
+            let g = d[i + 1]
+            let b = d[i + 2]
+            d[i] = (r * 0.393) + (g * 0.769) + (b * 0.189) // red
+            d[i + 1] = (r * 0.349) + (g * 0.686) + (b * 0.168) // green
+            d[i + 2] = (r * 0.272) + (g * 0.534) + (b * 0.131) // blue
+        }
+    },
+
+    // 阈值
+    yuzhi(imgData) {
+        let d = imgData.data;
+        for (var i = 0; i < d.length; i += 4) {
+            var r = d[i];
+            var g = d[i + 1];
+            var b = d[i + 2];
+
+            var v = (0.2126*r + 0.7152*g + 0.0722*b >= 60) ? 255 : 0;
+            d[i] = d[i+1] = d[i+2] = v;
+        }
+    },
+
+    // 动态模糊
+    muhu(imgData) {
+        let d = imgData.data;
+        let n = 20;
+        let m = n - 1 ;
+        for (var i = 0; i < d.length; i += 4) {
+            d[i] = (d[i] + d[i - 4] * m) / n
+            d[i + 1] = (d[i + 1] + d[i - 3] * m) / n
+            d[i + 2] = (d[i + 2] + d[i - 2] * m) / n
+            d[i + 3] = (d[i + 3] + d[i - 1] * m) / n
+        }
+    },
+
 })
--- main.js
+++ main.js
@@ -8,22 +8,6 @@ let img = new Image(),
     imgData = null,
     arrData = [];
 
-imgfile.onchange = function () {
-    let file = this.files[0],
-        reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function () {
-        img.src = this.result;
-        img.onload = function () {
-            canvas.width = img.width;
-            canvas.height = img.height;
-            ctx.drawImage(img,0,0);
-            imgData = ctx.getImageData(0,0,img.width,img.height);
-            arrData = imgData.data.slice();
-        }
-    }
-}
-
 arrFilterBtn.forEach((item,index)=>{
     item.onclick = function () {
         for(var i=0; i<arrData.length; i++){
@@ -31,9 +15,46 @@ arrFilterBtn.forEach((item,index)=>{
         };
         filter[index](imgData);
         ctx.putImageData(imgData,0,0);
-        download.href = canvas.toDataURL();
+        download.href = canvas.toDataURL('image/png');
     }
-})
+});
+
+imgfile.onchange = function () {
+    var file = this.files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+        img.src = this.result;
+        img.onload = function () {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img,0,0)
+            imgData = ctx.getImageData(0,0,img.width,img.height);
+            arrData = imgData.data.slice();
+        }
+    }
+}
+
+/*
+ * 1.实现本地预览图片
+ *       file接口 —— 选择图片
+ *           files[0]
+ *       FileReader —— 拿到图片数据
+ *           readAsDataURL
+ *           result
+ *       Image —— 把数据给Image载体
+ *       drawImage —— 图片显示在canvas上
+ * 2.拿到图片的数据
+ *       getImageData
+ * 3.修改图片数据
+ *       滤镜
+ * 4.把修改的数据重新渲染到canvas上
+ *       putImageData
+ * 5.下载修改好的图片
+ *       toDataURL
+ *       a标签的download属性
+ * */
+
 
 
 
